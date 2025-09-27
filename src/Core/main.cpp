75db665565cd2b9bbe4b983153702935f47a71b7
UgoMuto <ugomuto96@gmail.com>
2025-09-27 18:00:23 +0200

    Aggiornata risoluzione

--- src/Core/main.cpp
+++ src/Core/main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
 
-    Application app(DisplayConfig(sf::Vector2u(800, 600), 32), "Test Finestra SFML");
+    Application app(DisplayConfig(sf::Vector2u(1600, 900), 32), "Test Finestra SFML");
     app.run();
 
 
