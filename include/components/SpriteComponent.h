131a219a91caf52903aaaa2a5e5f6fdc5700398c
UgoMuto <ugomuto96@gmail.com>
2025-10-11 16:25:05 +0200

    Revert "componenti scomparsi"
    This reverts commit 6301ecd108a3d5dcd50a6c2f39258c5aeb5a6d8a.

---
+++ include/components/SpriteComponent.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <memory>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+
+struct SpriteComponent {
+    sf::Sprite sprite;                                 // L’oggetto disegnabile di SFML che contiene posizione, texture e transform
+    std::shared_ptr<sf::Texture> texture{ nullptr };   // Gestione sicura della texture condivisa da mantenere viva altrove (ResourceManager)
+   
+    bool visible{ true };            // Flag per abilitare/disabilitare il rendering di questa entità
+
+    // Costruttore di default
+    SpriteComponent() = default;
+
+    // Costruttore comodo per assegnare subito la texture
+    explicit SpriteComponent(const std::shared_ptr<sf::Texture> & tex) : texture(tex), sprite(*tex) {
+
+        if (texture) {
+            sprite.setTexture(*texture);
+        }
+    }
+};
+
+//// ESEMPIO: COME CARICARE UNA TEXTURE CON RESOURCE MANAGER?
+//
+// - Carico la texture nel ResourceManager e ottengo uno shared_ptr
+//  
+// auto playerTexture = resourceManager.getTexture("path/to/asset/player.png");
+//
+// - Creo il componente assegnando subito la texture
+//  
+// registry.emplace<SpriteComponent>(entity, playerTexture);
+// 
+// - Ora puoi accedere al componente
+// 
+// auto& spriteComp = registry.get<SpriteComponent>(player);
+//
+// spriteComp.sprite.setPosition(100.f, 200.f); // esempio di uso
+
